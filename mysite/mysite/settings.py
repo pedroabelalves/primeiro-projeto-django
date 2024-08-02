e5c2539bb21975b9775ce4013637307a58aff545
unknown <pedroabel679@gmail.com>
2024-08-02 19:53:04 -0300

    configuração adicionais do vercel

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'django-insecure-ms7cj5659ctlk!3!94xw8n$*fbp05db%262agygdw0=-=(4x3u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1','.vercel.app'
+]
 
 
 # Application definition
@@ -68,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'mysite.wsgi.app'
 
 
 # Database
